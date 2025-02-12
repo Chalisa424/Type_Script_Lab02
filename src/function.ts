25afc864858baff3640f439f5492a348c0d12a57
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-02-12 18:08:49 +0700

    Task4.2

---
+++ src/function.ts
@@ -0,0 +1,6 @@
+//Task4
+const add = (a:number,b:number):string => {
+    const result =  a+b;
+    return result.toString();
+ }//cannot find name add
+ 
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 //Task3
-const findMax = (numbers: number[]): string => {
-    if (numbers.length === 0) {
-        return "List is empty";
-    }
-    const maxNumber = Math.max(...numbers);
-    return "The maximum number is " + maxNumber;
-};
+// const findMax = (numbers: number[]): string => {
+//     if (numbers.length === 0) {
+//         return "List is empty";
+//     }
+//     const maxNumber = Math.max(...numbers);
+//     return "The maximum number is " + maxNumber;
+// };
 
-const numbers = [1, 2, 3, 4, 5];
-console.log(findMax(numbers)); // The maximum number is 5
+// const numbers = [1, 2, 3, 4, 5];
+// console.log(findMax(numbers)); // The maximum number is 5
+
+const result = add(1,2) + 0;
+console.log(result, 'type of result:', typeof result);//cannot find name add
+
