f5d900446792dad544ade49dabdfc3c6d36ddba6
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-02-12 17:46:53 +0700

    Task3.5

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 //Task3
-const add = (a :number ,b:number) : string => {
-    const result = a + b;
-    return result.toString();
-}
-const result = add(1,2) + 0;
-console.log(result, 'type of result:', typeof result);// 30 type of result: string
+const findMax = (numbers: number[]): string => {
+    if (numbers.length === 0) {
+        return "List is empty";
+    }
+    const maxNumber = Math.max(...numbers);
+    return "The maximum number is " + maxNumber;
+};
 
+const numbers = [1, 2, 3, 4, 5];
+console.log(findMax(numbers)); // The maximum number is 5
