c30aede4f324109217e81f11a3b82cbef6e79719
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-02-12 18:23:57 +0700

    Task4.5

--- src/function.ts
+++ src/function.ts
@@ -3,4 +3,11 @@ export const add = (a:number,b:number):string => {
     const result =  a+b;
     return result.toString();
  }//30 type of result: string
+
+ export const  subtract = (a:number,b:number):string => {
+    const result =  a-b;
+    return result.toString();
+ }//30 type of result: string and -10 type of result2: string
+
+
  
