ec3165ad1344e8e0eb46b7f96831f91b75c8f4d9
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-02-12 18:28:37 +0700

    Task4.6

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,10 @@
 // const numbers = [1, 2, 3, 4, 5];
 // console.log(findMax(numbers)); // The maximum number is 5
 
-import { add,subtract } from './function';
+import add, { subtract } from './function';
 const result = add(1,2) + 0;
 const result2 = subtract(1,2) + 0;
-console.log(result, 'type of result:', typeof result);//30 type of result: string
-console.log(result2, 'type of result2:', typeof result2);//30 type of result: string and -10 type of result2: string
+console.log(result, 'type of result:', typeof result);
+console.log(result2, 'type of result2:', typeof result2);
+//30 type of result: string and -10 type of result2: string
 
