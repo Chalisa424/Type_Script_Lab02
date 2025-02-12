da16e22f3788167652532d48eecafe9b58ada947
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-02-12 17:41:31 +0700

    Task3.4

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 //Task3
 const add = (a :number ,b:number) : string => {
-    return a + b;
+    const result = a + b;
+    return result.toString();
 }
 const result = add(1,2) + 0;
-console.log(result, 'type of result:', typeof result);//Type Error: Type 'number' is not assignable to type 'string'
+console.log(result, 'type of result:', typeof result);// 30 type of result: string
+
