eefcce844fe0cea9291722c02af48201cc5b9be2
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-02-12 17:29:06 +0700

    Task3.1

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
-//Task2
-let x: number | string = 10;
-x = 'hello'; 
-if (typeof x === 'string') {
-    console.log('x is a string');
-}else if (typeof x === 'number') {
-    console.log('x is a number');  
-}else {
-    console.log('x is neither a string nor a number');
+//Task3
+const add = (a,b) => {
+    return a + b;
 }
-// x is a string
+console.log(add(1,2)); //Parameter a and b have any type
