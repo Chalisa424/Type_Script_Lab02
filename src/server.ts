d223bc45d2360289064dfb44147a94f95a5c06ed
Chalisa424 <chalisa_wongsa@cmu.ac.th>
2025-02-13 01:30:15 +0700

    Task8.8

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,3 @@
-//Task8
 import express, { Request, Response } from 'express'
 const app = express()
 const port = 3000
@@ -7,6 +6,11 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
 })
 
+// เพิ่มการเรียกใช้ mapping URL /test
+app.get('/test', (req: Request, res: Response) => {
+  res.send('Hello World! 3')
+})
+
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
-})
+})//แสดงผลลัพธ์ในAPI DOG คือ Hello World! 3
